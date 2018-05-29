88150777db3f4701da03d0c71f78c89230e21a41
biernat <sbiernacki4@gmail.com>
2018-05-29 21:12:12 +0200

    First commit

---
+++ src/main/java/com/biernat/currencyconverter/CurrencyconverterApplication.java
@@ -0,0 +1,14 @@
+package com.biernat.currencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyconverterApplication
+{
+    
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CurrencyconverterApplication.class, args);
+    }
+}
