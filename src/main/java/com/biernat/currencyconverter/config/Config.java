23ce49d92a76fc4f2d6cee773d71b994d5d7e7d7
biernat <sbiernacki4@gmail.com>
2018-06-01 20:53:32 +0200

    Add config class

---
+++ src/main/java/com/biernat/currencyconverter/config/Config.java
@@ -0,0 +1,40 @@
+package com.biernat.currencyconverter.config;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config
+{
+    
+    @Bean
+    public RestTemplate restTemplate()
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(0, mappingJackson2HttpMessageConverter());
+        
+        return restTemplate;
+    }
+    
+    @Bean
+    public ObjectMapper objectMapper()
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+        
+        return mapper;
+    }
+    
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter()
+    {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper());
+        
+        return converter;
+    }
+}
