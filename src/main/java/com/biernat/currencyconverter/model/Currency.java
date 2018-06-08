f6b83d15971950512d9b22d7349df648e9f43232
biernat <sbiernacki4@gmail.com>
2018-06-08 12:18:59 +0200

    Adding Currency class.

---
+++ src/main/java/com/biernat/currencyconverter/model/Currency.java
@@ -0,0 +1,26 @@
+package com.biernat.currencyconverter.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class Currency
+{
+    private String base;
+    private String date;
+    private Map<String, Double> rates = new HashMap<>();
+    
+    public void setRate(String currencyCode, double rate)
+    {
+        this.rates.put(currencyCode, rate);
+    }
+    
+    public double getRate(String currencyCode)
+    {
+        return this.rates.get(currencyCode);
+    }
+}
